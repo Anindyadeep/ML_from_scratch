4934b3c5be0c02b70125e66bd226848d3b700513
Anindya <2005641@kiit.ac.in>
2021-06-21 22:49:05 +0530

    starting polynomial regression

--- Regression/multiple_linear_regression.py
+++ Regression/multiple_linear_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from numpy.core.fromnumeric import shape
 
 class MultipleLinearRegression:
@@ -11,7 +12,16 @@ class MultipleLinearRegression:
             self.y = y
         
         self.m = int(self.X.shape[1])
-        self.W = np.random.randn(1, len(self.X))
+        '''
+        limit = 1 / math.sqrt(n_features)
+        self.w = np.random.uniform(-limit, limit, (n_features, )) # can use this
+        '''
+        #self.W = np.random.randn(1, len(self.X))
+
+        n_features = int(self.X.shape[0])
+        limit = 1/math.sqrt(n_features)
+        self.W = np.random.uniform(-limit, limit, (1, n_features))
+
         self.b = np.zeros(shape=(1,1))
         self.history = {'loss': []}
         self.count = 0
@@ -49,6 +59,8 @@ class MultipleLinearRegression:
             automatically by the algorithm such that if the loss at nth epoch (last) > loss at (n+1)th epoch
             the it would run for more steps, tuning the leraning rate as well
             '''
+            '''TODO: IMP DO THE LOSS AND ALL w.r.t THE VALIDATION LOSS'''
+            
             w_grad, b_grad = self._compute_grads()
             self.W -= learning_rate * w_grad 
             self.b -= learning_rate * b_grad
@@ -56,6 +68,7 @@ class MultipleLinearRegression:
             if epoch % 50 == 0 and show_history:
                 if epoch % 50 == 0: print(f"After epoch {epoch} loss : {self._MSE()}, weights: {self.W}, bias: {self.b}")
             self.history['loss'].append(int(self._MSE()))
+
 
     def train_recursive(self, learning_rate = 0.003):
         loss_before = self._MSE()
@@ -70,33 +83,35 @@ class MultipleLinearRegression:
         self.count += 1
         if self.count % 2 == 0: print(f"after {self.count} loss: {self._MSE()}")
         
-        if loss_before - loss_after > 0.01 : self.train_recursive()
+        if abs(loss_before - loss_after) == 0 : self.train_recursive()
         else: return
+
+
 
 if __name__ == '__main__':
     from sklearn import datasets
     from sklearn.model_selection import train_test_split
     from sklearn.linear_model import LinearRegression
 
-    X, y = datasets.make_regression(n_samples=600, n_features=3, noise=20, random_state=4)
+    n_features = 10
+    X, y = datasets.make_regression(n_samples=600, n_features=n_features, noise=20, random_state=4)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
     y_train = y_train.reshape(len(y_train), 1)
-    X_test = X_test.reshape(len(X_test), 3)
+    X_test = X_test.reshape(len(X_test), n_features)
     y_test = y_test.reshape(len(y_test), 1)
 
-    print(X_train.shape, y_train.shape)
-    print(X_test.shape, y_test.shape)
+    #print(X_train.shape, y_train.shape)
+    #print(X_test.shape, y_test.shape)
 
     mlr = MultipleLinearRegression(X_train, y_train)
-    mlr.train(500, 0.03)
-    mlr.train_recursive(0.003)
+    mlr.train(200, 0.03)
 
     model = LinearRegression().fit(X_train, y_train)
     pred_model = model.predict(X_test)
 
     pred_mlr = mlr.predict(X_test)
-    print(pred_model.shape, pred_mlr.shape)
+    #print(pred_model.shape, pred_mlr.shape)
 
     l_model = mlr.loss(pred_model, y_test)
     l_mlr = mlr.loss(pred_mlr, y_test)
